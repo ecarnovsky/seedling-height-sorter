908d8f9f2dd9f60949ccf2ce797e4a24fd059a92
ecarnovsky <ecarnovsky@gmail.com>
2024-02-05 15:35:51 -0500

    Added the plants.js and sorted-plant-list.js files

---
+++ js/plant.js
@@ -0,0 +1,8 @@
+class Plant {
+
+    constructor(name, height, unit){
+        this.name = name
+        this.height = height
+        this.unit = unit
+    }
+}
---
+++ js/sorted-plant-list.js
@@ -0,0 +1,5 @@
+class SortedPlantList{
+    constructor(){
+        this.plantsArr = []
+    }
+}
